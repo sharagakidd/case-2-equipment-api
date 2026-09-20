d915270dfbbcfdc129404fbef1bf8d95f7bb1950
Andrew <artemenko.0550@mail.ru>
2026-09-20 17:39:28 +0300

    feat(validators): validate params on id-based routes

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import { requestController } from '../controllers/requestController.js';
 import { validate } from '../middlewares/validate.js';
-import { createRequestSchema, updateRequestSchema, statusChangeSchema } from '../validators/requestSchemas.js';
+import {
+  createRequestSchema,
+  updateRequestSchema,
+  statusChangeSchema,
+  idParamSchema,
+} from '../validators/requestSchemas.js';
 import { requestListQuery } from '../validators/querySchemas.js';
 
 // Роутер ресурса «заявка». Монтируется по пути /requests, поэтому пути внутри
@@ -11,9 +16,9 @@ const router = Router();
 
 router.get('/', validate({ query: requestListQuery }), requestController.list);
 router.post('/', validate({ body: createRequestSchema }), requestController.create);
-router.patch('/:id/status', validate({ body: statusChangeSchema }), requestController.changeStatus);
-router.get('/:id', requestController.getOne);
-router.patch('/:id', validate({ body: updateRequestSchema }), requestController.update);
-router.delete('/:id', requestController.remove);
+router.patch('/:id/status', validate({ params: idParamSchema, body: statusChangeSchema }), requestController.changeStatus);
+router.get('/:id', validate({ params: idParamSchema }), requestController.getOne);
+router.patch('/:id', validate({ params: idParamSchema, body: updateRequestSchema }), requestController.update);
+router.delete('/:id', validate({ params: idParamSchema }), requestController.remove);
 
 export default router;
